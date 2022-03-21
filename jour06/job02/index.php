11ca28bb519fdf4a66fddd6391fad9cde21048ec
salim-ouari <salim.ouari@laplateforme.io>
2022-03-21 11:02:03 +0100

    job02

---
+++ jour06/job02/index.php
@@ -0,0 +1,25 @@
+<!-- Créez un fichier index.php contenant les balises html de base (doctype,
+html, head, body). Créez un fichier style.css et ajouter des propriétés
+permettant à votre body d’avoir une couleur de background #88c1d0.
+A l’aide des media queries, cette couleur doit évoluer de la façon suivante :
+- #ffca4b lorsque la résolution est comprise entre 1680 et 1920
+- #275fa0 lorsque la résolution est comprise entre 1280 et 1680
+
+- #ffffff lorsque la résolution est inférieure à 1280 -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
