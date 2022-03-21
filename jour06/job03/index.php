c5ee2a17dc8e3bafb37c44c397d645eaa745573f
salim-ouari <salim.ouari@laplateforme.io>
2022-03-21 11:31:51 +0100

    job03

---
+++ jour06/job03/index.php
@@ -0,0 +1,26 @@
+<!-- Créez un fichier index.php contenant les balises html de base (doctype,
+html, head, body). Créez un fichier style.css qui contiendra le style de cette
+page. Dans votre body, créez 4 sections ayant comme classe “section”.
+Mettez la couleur #88c1d0 en background de vos .section. Ajoutez des
+medias queries de sorte à respecter le schéma suivant : -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <section class="section"></section>
+    <section class="section"></section>
+    <section class="section"></section>
+    <section class="section"></section>
+
+</body>
+
+</html>
