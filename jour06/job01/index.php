d9d1e071f3b84f38bfa6e0579a4658a53980c13b
salim-ouari <salim.ouari@laplateforme.io>
2022-03-21 10:47:15 +0100

    job01

---
+++ jour06/job01/index.php
@@ -0,0 +1,39 @@
+<!-- Créez un fichier index.php contenant les balises html de base (doctype,
+html, head, body). Créez quatre fichiers de style :
+- style1.css : modifie la couleur du background en #88c1d0.
+- style2.css : modifie la couleur du background en #ffca4b.
+- style3.css : modifie la couleur du background en #275fa0.
+- style4.css : modifie la couleur du background en #ffffff.
+Dans votre code html, linkez la bonne feuille de style en fonction de la
+largeur de l’écran de l’utilisateur : style1.css par défaut, style2.css lorsque
+la résolution est comprise entre 1680px et 1920px, style3.css entre 1280px
+et 1680px, et style4.css lorsqu’elle est inférieure à 1280. -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- style1 -->
+    <link rel="stylesheet" href="style1.css">
+
+    <!-- style2 -->
+    <link rel="stylesheet" href="style2.css">
+
+    <!-- style3 -->
+    <link rel="stylesheet" href="style3.css">
+    <!-- style4 -->
+    <link rel="stylesheet" href="style4.css">
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
